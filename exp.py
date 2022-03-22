2faba9a2b80e4c78ab293199d0841828086e692b
zxcv <asdf@qwer.com>
2022-03-22 20:51:50 +0100

    exp: devreg

--- exp.py
+++ exp.py
@@ -3,12 +3,30 @@
 import socket
 import struct
 
+DEVREG = 0x00
+
+def p8(i: int):
+    i &= 0xff
+    return struct.pack('<B', i)
+
 def p16(i: int):
     i &= 0xffff
-
+    return struct.pack('<H', i)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('localhost', 34756))
 
-s.sendall(b'\x00' + b'\x05\x00' + b'ASDFG' + b'\x02\x00' + b'\x04' + b'zxcv' + b'\x05' + b'qwer')
-print(s.recv(4096))
+def devreg(sock, name, fields):
+    msg = p8(DEVREG) + p16(len(name)) + name + p16(len(fields))
+
+    for field in fields:
+        msg += p8(len(field)) + field
+
+    sock.sendall(msg)
+    return s.recv(4096)
+
+#s.sendall(b'\x00' + b'\x05\x00' + b'ASDFG' + b'\x02\x00' + b'\x04' + b'zxcv' + b'\x05' + b'qwer')
+#print(s.recv(4096))
+
+print(devreg(sock = s, name = b'ASDFGH\x00QWER', fields = (b'zxcv', b'qwer')))
+
--- sensconnect.c
+++ sensconnect.c
@@ -212,7 +212,7 @@ int handle_packet(int s, struct sockaddr_in *client, uint8_t *packet_buff, ssize
 
             msg.code = RESP_SUCCESS;
             resp_status(s, client, msg);
-            puts("DEVREG success");
+            printf("[STATUS] Registered new device: %s\n", ndev->e_dev.name);
 
             break;
         }
@@ -405,7 +405,7 @@ int main(void)
 
         packet_buff[recv_len] = '\0';
 
-        printf("Received packet from %s:%d\n",
+        printf("[STATUS] Received packet from %s:%d\n",
                inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 
         handle_packet(serv_sock, &client, packet_buff, recv_len);
